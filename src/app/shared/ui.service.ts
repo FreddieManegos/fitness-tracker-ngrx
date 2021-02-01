fa806a676fa516bd4f783aeae98c815961e68f43
Freddie Mar A. Manegos <freddiemar.manegos@g.msuiit.edu.ph>
2021-02-01 15:48:21 +0800

    added UI Service

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class UIService {
   loadingStateChanged = new Subject<boolean>();
-  constructor() { }
+  constructor(private snackbar: MatSnackBar) { }
 
+  showSnackbar(message, action, duration) {
+    this.snackbar.open(message, action, {
+      duration: duration
+    });
+  }
 }
